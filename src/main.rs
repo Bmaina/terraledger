bbc0e35cf20ce592f12f88f5ad16a09285b2b85b
Bmaina <maina.anu@gmail.com>
2026-04-07 19:13:33 +0300

    Add continuous loop for Render deployment

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() {
-    println!("TerraLedger is running!");
+    // Get the port Render expects, default to 8080 locally
+    let port = env::var("PORT").unwrap_or("8080".to_string());
+    let addr: SocketAddr = format!("0.0.0.0:{}", port).parse().unwrap();
+
+    // Bind a TCP listener to the port
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("TerraLedger is running on port {}", port);
+
+    // Keep the app running
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            // For now, we just accept connections but do nothing
+            drop(socket);
+        });
+    }
 }
