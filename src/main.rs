cb42077b089de2415c8c3dc259c7e48bc1ff0e23
Bmaina <maina.anu@gmail.com>
2026-04-07 19:50:40 +0300

    Convert to simple HTTP server for Render

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-use std::env;
-use std::net::SocketAddr;
-
-use tokio::io::AsyncReadExt;
-use tokio::net::TcpListener;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Render provides the port via $PORT, default to 8080 locally
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     let addr: SocketAddr = format!("0.0.0.0:{}", port).parse()?;
 
@@ -16,12 +9,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         let (mut socket, _) = listener.accept().await?;
 
-        // This is the line that was causing the new error
         tokio::spawn(async move {
             let mut buffer = [0u8; 1024];
 
-            // Read and ignore for now
-            let _ = socket.read(&mut buffer).await;
+            // Simple HTTP response
+            if let Ok(_) = socket.read(&mut buffer).await {
+                let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 23\r\n\r\nHello from TerraLedger!\n";
+                let _ = socket.write_all(response.as_bytes()).await;
+                let _ = socket.flush().await;
+            }
         });
     }
 }
